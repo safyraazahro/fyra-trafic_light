9ceefb126ea80db087c967db3d89fef1112e149e
safyraazahro <safyrasaya@gmail.com>
2026-01-07 07:19:37 -0800

    Initial commit
    Implement traffic light switch functionality with reset.

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const switchBtn = document.getElementById("switchBtn");
 
+function resetLights() {
+	red.style.background = "#444";
+	yellow.style.background = "#444";
+	green.style.background = "#444";
+}
+
+let current = "red";
+red.style.background = "red";
+
+function switchLight() {
+	switchBtn.disabled = true;
+	resetLights();
+
+	// Nyalain kuning dulu
+	yellow.style.background = "yellow";
+
+	setTimeout(() => {
+		resetLights();
+
+		if (current === "red") {
+			green.style.background = "lime";
+			current = "green";
+		} else {
+			red.style.background = "red";
+			current = "red";
+		}
+
+		switchBtn.disabled = false;
+	}, 1000); // kuning nyala 300ms
+}
+
+switchBtn.addEventListener("click", switchLight);
